682505850f8d6a710b41a90d682d7e831ddfa010
Mouad-Ghour <mouadghourdou18@gmail.com>
2023-07-23 23:33:59 +0100

    added the main generator to my app

--- Components/QuoteGenerator/QuoteGenerator.tsx
+++ Components/QuoteGenerator/QuoteGenerator.tsx
@@ -20,3 +20,162 @@ export const GradientBackgroundCon = styled.div`
         }
     }
 `;
+
+
+export const BackgroundImage1 = styled(Image)`
+    position: fixed;
+    z-index: 1;
+    margin-left: 5%;
+    margin-top: 0px;
+  
+`;
+
+
+
+export const BackgroundImage2 = styled(Image)`
+    position: fixed;
+    z-index: 1;
+    right: 5%;
+    bottom: -10px;
+  
+`;
+
+export const FooterCon = styled.div`
+    width: 100vw;
+    height: 10vh;
+    text-align: center;
+    font-weight: bolder;
+    font-size: 15px;
+    font-family: 'Source Code Pro', monospace;
+    color: black;
+    z-index: 999999;
+    bottom: 0;
+    position: absolute;
+
+    &:hover {
+        color: black;
+    }
+    
+`;
+
+
+export const FooterLink = styled(Link)`
+    color: white;
+    text-decoration: none;
+    
+    &:hover {
+        color: black;
+    }
+`;
+
+export const QuoteGenratorCon = styled.div`
+    
+    min-height: 350px;
+    min-width: 350px;
+    height: 70vh;
+    width: 70vw;
+    border: 2px solid #16ffec;
+    border-radius: 15px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    z-index: 2;
+
+
+    background: rgba( 0, 255, 182, 0.2 );
+    box-shadow: 0 8px 32px 0 #16ffec;
+    backdrop-filter: blur( 3.5px );
+    -webkit-backdrop-filter: blur( 3.5px );
+    border-radius: 10px;
+    border: 1px solid rgba( 255, 255, 255, 0.18 );
+
+`;
+
+export const QuoteGeneratorInnerCon = styled.div`
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    width: 100%;
+`;
+
+export const QuoteGeneratorTitle = styled.div`
+
+    font-family: 'Permanent Marker', cursive;
+    font-size: 50px;
+    text-align: center;
+    color: white;
+    padding: 0px 20px 0px 20px;
+    position: relative;
+    /* media query */
+    @media only screen and (max-width: 600px) {
+        font-size: 30px;
+    }
+
+`;
+
+
+export const QuoteGeneratorSubTitle = styled.div`
+    
+    color: white;
+    font-family: 'Caveat', cursive;
+    font-size: 35px;
+    position: relative;
+    width: 100%;
+    text-align: center;
+    padding: 0px 20px 0px 20px;
+    /* media query */
+    @media only screen and (max-width: 600px) {
+        font-size: 25px;
+    }
+
+`;
+
+
+export const QuoteGeneratorButton = styled.div`
+
+    height: 100px;
+    width: 300px;
+    border: 2px solid #16ffec;
+    border-radius: 20px;
+
+    margin-top: 20px;
+    position: relative;
+    transition: 0.2s all ease-in-out;
+    cursor: pointer;
+    top: 20px;
+    margin: auto;
+    transform-origin: center;
+
+    background: rgba( 0, 0, 70, 0.3 );
+    box-shadow: 0 8px 32px 0 #ffffff;
+    backdrop-filter: blur( 20px );
+    -webkit-backdrop-filter: blur( 20px );
+    border-radius: 10px;
+    border: 1px solid white;
+    &:hover {
+        /* filter: brightness(3);
+        transition: 0.2s all ease-in-out;
+        transform: scale(1.1);
+        background-color: #16ffec;
+
+        transform-origin: center; */
+    }
+`;
+
+
+export const GenerateQuoteButtonText = styled.div`
+ 
+    color: white;
+    font-family: 'Caveat', cursive;  
+    font-size: 35px;
+    left: 50%;
+    top: 50%;   
+    transform: translate(-50%, -50%);
+    position: absolute;
+    width: 100%;
+    text-align: center;
+
+
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/Components/QuoteGenerator/QuoteGenerator'
+import { useState } from 'react';
 
+//Components
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GenerateQuoteButtonText, GradientBackgroundCon, QuoteGeneratorButton, QuoteGeneratorInnerCon, QuoteGeneratorSubTitle, QuoteGeneratorTitle, QuoteGenratorCon } from '@/Components/QuoteGenerator/QuoteGenerator'
+
+
+//Images
+import Clouds1 from "../assets/cloud_1.png"
+import Clouds2 from "../assets/cloud_2.png"
+
+
+//exports
 
 export default function Home() {
+
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0); 
+
   return (
     <>
       <Head>
@@ -13,11 +26,78 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-  
+      {/*Quotes Generator Modal Pop-up*/}
+      {/* {<QuoteGeneratorModal
+      />} */}
+
+      {/*Quotes Generator*/}
+      <QuoteGenratorCon>
+
+        <QuoteGeneratorInnerCon>
+
+          <QuoteGeneratorTitle>
+
+            Quotes Generator
+
+          </QuoteGeneratorTitle>
+
+          <QuoteGeneratorSubTitle>
+
+              Generate random quotes from famous people
+
+          </QuoteGeneratorSubTitle>
+
+          <QuoteGeneratorButton>
+
+            <GenerateQuoteButtonText >
+
+              Generate Quote
+
+            </GenerateQuoteButtonText>
+
+          </QuoteGeneratorButton>
+
+        </QuoteGeneratorInnerCon>
+
+      </QuoteGenratorCon>
+
+
+
       {/*Background*/}
-      <GradientBackgroundCon>
+      <GradientBackgroundCon> 
+
+        {/*Bakcground Images*/}
+        <BackgroundImage1
+          src={Clouds1}
+          height="300"
+          alt="CloudyBakcgournd1"
+          />
+
+        <BackgroundImage2
+          src={Clouds2}
+          height="300"
+          alt="CloudyBakcgournd2"
+          />
+
+        {/*Footer*/}
+          <FooterCon>
+            <>
+              Quotes Generated: {numberOfQuotes}
+              <br/>
+              Developed with next by  &nbsp; 
+              <FooterLink
+                href="https://www.linkedin.com/in/mouadghourdou/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+               @MouadGh
+              </FooterLink>
+
+            </>
+          </FooterCon>
+
       </GradientBackgroundCon>
 
     </>
-  )
+    )
 }
